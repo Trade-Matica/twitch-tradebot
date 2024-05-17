6f01489375253bb30834a079f67c1592e280840b
nikolahellatrigger <0xF7A4C6@proton.me>
2024-05-17 05:21:59 +0200

    market + fees check + order_size

--- README.txt
+++ README.txt
@@ -1,11 +1,24 @@
-Allow your viewers to trade bitcoin on your account.
+This bot allows your viewers to trade Bitcoin on your account by using a simple chat command.
 
 Usage:
-    - !trade <symbol> <SIDE> <ENTRY_PRICE> <TP_PRICE> <SL_PRICE>
+To place a trade, use the following command in chat:
+    - !trade <symbol> <SIDE> <TP_PRICE> <SL_PRICE> [ENTRY_PRICE]
+
+Parameters:
+    - <symbol>: The trading symbol, e.g., BTC.
+    - <SIDE>: The side of the trade, either BUY or SELL.
+    - <TP_PRICE>: The take profit price.
+    - <SL_PRICE>: The stop loss price.
+    - [ENTRY_PRICE]: The entry price (optional for market orders).
 
 Example:
-    - !trade BTC BUY 64400 64500 64300
+To place a limit order:
+    - !trade BTC BUY 65000 64900 64910
 
-Requirement:
-    - Minimum risk reward: 2R.
-    - Order price must be 1% around the market price.
+To place a market order:
+    - !trade BTC BUY 64500 64300
+
+Requirements:
+    - Minimum Risk Reward Ratio: The trade must have a minimum risk-reward ratio of 2:1.
+    - Order Price Range: For limit orders, the entry price must be within 1% of the current market price.
+    - Order Value: The order value is dynamically calculated to ensure the trade size fits the specified USDT amount.
--- main.py
+++ main.py
@@ -5,17 +5,21 @@ from pybitget.utils import random_string
 from pybitget.enums import *
 import os
 
+__MIN_RR__ = 2.0
+__MIN_PC_PRICE__ = 0.99
+__ORDER_VALUE__ = 0.618 * 125  # order_cost * laverage | = 0.001 BTC
+
+
 class Bot(commands.Bot):
     def __init__(self):
+        load_dotenv()
 
         self.prefix = os.getenv("PREFIX")
 
         super().__init__(
             token=os.getenv("ACCESS_TOKEN"),
             prefix=self.prefix,
-            initial_channels=[
-                os.getenv("CHANNEL"),
-            ],
+            initial_channels=[os.getenv("CHANNEL")],
         )
 
         self.bitget = Client(
@@ -24,6 +28,9 @@ class Bot(commands.Bot):
             os.getenv("BITGET_API_PASSPHRASE"),
             use_server_time=False,
         )
+
+        self.maker_fee = float(os.getenv("MAKER_FEE"))
+        self.taker_fee = float(os.getenv("TAKER_FEE"))
 
     async def event_ready(self):
         print(f"Logged in as | {self.nick}")
@@ -35,18 +42,23 @@ class Bot(commands.Bot):
 
         args = message.content[len(self.prefix) :].split()
 
-        if args[0] != "trade" or len(args) != 6:
+        if args[0] != "trade" or len(args) < 5:
             return
 
-        symbol, side, entry_price, tp_price, sl_price = args[1:6]
+        symbol, side, tp_price, sl_price = args[1:5]
+        entry_price = None
+
+        if len(args) == 6:
+            entry_price = args[5]
 
         if symbol not in ["BTC"]:
             return
 
         try:
-            entry_price = float(entry_price)
             tp_price = float(tp_price)
             sl_price = float(sl_price)
+            if entry_price:
+                entry_price = float(entry_price)
         except ValueError:
             print("Invalid price format")
             return
@@ -62,20 +74,29 @@ class Bot(commands.Bot):
 
         symb = f"S{symbol}SUSDT_SUMCBL"
 
-        if not self.validate_price(symb, entry_price, tp_price, sl_price):
+        if entry_price and not self.validate_price(
+            symb, entry_price, tp_price, sl_price
+        ):
+            return
+
+        if not self.is_trade_profitable(symb, side, entry_price, tp_price):
+            print("Trade is not profitable after fees.")
             return
 
         self.place_order(symb, clean_side, entry_price, tp_price, sl_price)
         print(f"Trade order placed: {message.content}")
 
     def validate_price(self, symbol, entry_price, tp_price, sl_price):
-        price = float(self.bitget.mix_get_market_price(
-            symbol=symbol,
-        )['data']['markPrice'])
+        price = float(
+            self.bitget.mix_get_market_price(symbol=symbol)["data"]["markPrice"]
+        )
+
+        if entry_price == None:
+            entry_price = price
 
         print("market price:", price)
 
-        if entry_price < price * 0.99:
+        if entry_price < price * __MIN_PC_PRICE__:
             print(
                 f"Entry price {entry_price} is less than 1% below the market price {price}"
             )
@@ -84,32 +105,76 @@ class Bot(commands.Bot):
         risk = abs(entry_price - sl_price)
         reward = abs(tp_price - entry_price)
 
-        if risk == 0 or reward / risk < 2:
+        if risk == 0 or reward / risk < __MIN_RR__:
             print(f"Invalid risk-reward ratio: reward {reward}, risk {risk}")
             return False
-        
-        print('RR:', reward / risk)
+
+        print("RR:", reward / risk)
 
         return True
 
     def validate_trade_parameters(self, side, entry_price, tp_price, sl_price):
         if side == "BUY":
-            if entry_price < tp_price and sl_price < entry_price:
+            if entry_price is None or (
+                entry_price < tp_price and sl_price < entry_price
+            ):
                 return NEW_BUY
         elif side == "SELL":
-            if tp_price < entry_price and entry_price < sl_price:
+            if entry_price is None or (
+                tp_price < entry_price and entry_price < sl_price
+            ):
                 return NEW_SELL
         return None
 
+    def is_trade_profitable(self, symbol, side, entry_price: float, tp_price: float):
+        entry_fee = 0
+
+        if entry_price is None:
+            entry_price = float(
+                (self.bitget.mix_get_market_price(symbol=symbol)["data"]["markPrice"])
+            )
+            entry_fee = entry_price * self.taker_fee
+        else:
+            entry_fee = entry_price * self.maker_fee
+
+        if side == "BUY":
+            exit_fee = tp_price * self.maker_fee
+            total_fee = entry_fee + exit_fee
+            position_size = __ORDER_VALUE__ / entry_price
+            profit = (tp_price - entry_price - total_fee) * position_size
+
+        elif side == "SELL":
+            exit_fee = tp_price * self.maker_fee
+            total_fee = entry_fee + exit_fee
+            position_size = __ORDER_VALUE__ / entry_price
+            profit = (entry_price - tp_price - total_fee) * position_size
+
+        print("Expected profits:", profit)
+        return profit > 0
+
+    def calculate_order_size(self, price):
+        return __ORDER_VALUE__ / price
+
     def place_order(self, symbol, side, entry_price, tp_price, sl_price):
         try:
+            if entry_price:
+                order_type = "limit"
+                order_size = self.calculate_order_size(entry_price)
+            else:
+                order_type = "market"
+                market_price = float(
+                    self.bitget.mix_get_market_price(symbol=symbol)["data"]["markPrice"]
+                )
+                order_size = self.calculate_order_size(market_price)
+
+            print(order_size)
             placed = self.bitget.mix_place_order(
                 symbol,
                 "SUSDT",
-                "0.001",
+                str(order_size),
                 side,
-                "limit",
-                price=entry_price,
+                order_type,
+                price=entry_price if entry_price else None,
                 clientOrderId=random_string("Cuongitl"),
                 presetStopLossPrice=sl_price,
                 presetTakeProfitPrice=tp_price,
@@ -121,6 +186,5 @@ class Bot(commands.Bot):
 
 
 if __name__ == "__main__":
-    load_dotenv()
     bot = Bot()
     bot.run()
